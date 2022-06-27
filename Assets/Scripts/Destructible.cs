0dbf1f7784954bc2b42316e9781a9ec977b8a089
manhpdhe151290 <manhpdhe151290@fpt.edu.vn>
2022-06-27 17:32:35 +0700

    ItemPickup

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Destructible : MonoBehaviour
 {
     public float destructionTime = 1f;
+       [Range(0f, 1f)]
+    public float itemSpawnChance = 0.2f;
+    public GameObject[] spawnableItems;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject,destructionTime);
     }
 
-    // Update is called once per frame
-    void Update()
+     private void OnDestroy()
     {
-        
+        if (spawnableItems.Length > 0 && Random.value < itemSpawnChance)
+        {
+            int randomIndex = Random.Range(0, spawnableItems.Length);
+            Instantiate(spawnableItems[randomIndex], transform.position, Quaternion.identity);
+        }
     }
+
 }
