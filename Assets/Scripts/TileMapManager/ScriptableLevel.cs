f7fb65693fc63699eef71600adf61031caf04062
TrungHieu0803 <hieunthe15001@fpt.edu.vn>
2022-07-04 19:05:27 +0700

    update: map and spawn bomb position

--- Assets/Scripts/TileMapManager/ScriptableLevel.cs
+++ Assets/Scripts/TileMapManager/ScriptableLevel.cs
@@ -9,6 +9,9 @@ public class ScriptableLevel : ScriptableObject
     public int LevelIndex;
     public List<SavedTile> GroundTiles;
     public List<SavedTile> UnitTiles;
+    public List<SavedTile> FrontOfPlayer;
+    public List<SavedTile> GrondOverlay;
+    public List<SavedTile> Obstacle;
 }
 
 [Serializable]
